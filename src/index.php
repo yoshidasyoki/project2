64b3e44db39cf3c5dff3ec40567e45e3f3b89767
yoshida <programming.study2000@mail.com>
2025-07-14 19:36:26 +0900

    Add phpdotenv

--- src/index.php
+++ src/index.php
@@ -1,6 +1,15 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-$mysqli = new mysqli('db', 'dev_user', 'pass', 'log');
+$hostname = 'db';
+$user = $_ENV['MYSQL_USER'];
+$pass = $_ENV['MYSQL_PASSWORD'];
+$database = $_ENV['MYSQL_DATABASE'];
+
+$mysqli = new mysqli($hostname, $user, $pass, $database);
 if($mysqli->connect_error) {
     echo '接続失敗'.PHP_EOL;
     exit();
